198379d336cac1f398342cc53109993bf887293b
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-12 18:28:16 -0700

    Press Start text flickers whenever the play clicks enter

---
+++ Assets/_scripts/StartGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class StartGame : MonoBehaviour {
+
+    public Text changeText;
+
+    void Start()
+    {
+       //nothing happens
+    }
+
+    public IEnumerator FlickerText()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            changeText.color = Color.black;
+            yield return new WaitForSeconds(0.1f);
+            changeText.color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(FlickerText());
+        }
+	}
+}
