4f2ab6deb85b4570255bccf12063c2be7f2ce1b9
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-12 20:05:21 -0700

    Added sound to the starting screen and started on the stage select

--- Assets/_scripts/StartGame.cs
+++ Assets/_scripts/StartGame.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
     public Text changeText;
+    public AudioClip impact;
+    AudioSource audioSource;
 
     void Start()
     {
-       //nothing happens
+        //nothing happens
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public IEnumerator FlickerText()
+    public IEnumerator Transition()
     {
+        audioSource.Play();
         for (int i = 0; i < 6; ++i)
         {
             changeText.color = Color.black;
@@ -22,12 +27,14 @@ public class StartGame : MonoBehaviour {
             changeText.color = Color.white;
             yield return new WaitForSeconds(0.1f);
         }
+        SceneManager.LoadScene("stage_select");
     }
 
     void Update () {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            StartCoroutine(FlickerText());
+            StartCoroutine(Transition());
+            
         }
 	}
 }
