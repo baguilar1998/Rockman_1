020d1f4d32c4b867cfa32aab949d6ffe224731b1
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-12 17:53:09 -0700

    Created a static start screen with debug code

---
+++ Assets/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    void Start()
+    {
+        print("game started");
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Enter was pressed");
+        }
+	}
+}
