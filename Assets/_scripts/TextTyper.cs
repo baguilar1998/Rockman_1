da7e3fa414a43d9ed8923817d6ad286d0e8a6ef2
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-13 18:17:07 -0700

    Finished Cutman Stage Select Animation

---
+++ Assets/_scripts/TextTyper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+using UnityEngine.Animations;
+
+public class TextTyper : MonoBehaviour {
+
+    public Text currentText;
+    public string displayText;
+
+	// Use this for initialization
+	void Start () {
+        currentText = GetComponent<Text>();
+        currentText.enabled = false;
+        StartCoroutine(TypeText());
+    }
+	
+    public IEnumerator TypeText()
+    {
+        yield return new WaitForSeconds(3f);
+        if (currentText.text.Equals("Cutman"))
+        {
+            currentText.enabled = true;
+            currentText.text = "";
+            displayText = "Cutman";
+            for(int i = 0; i < displayText.Length; ++i)
+            {
+                yield return new WaitForSeconds(0.2f);
+                currentText.text = currentText.text + displayText.Substring(i, 1);
+            }
+            
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {}
+}
