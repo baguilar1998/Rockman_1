0aaefd0d0445f3a013e7ef2694c9aacc353023ec
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-18 17:29:39 -0700

    Working on controls for Megaman

--- Assets/_scripts/MegamanController.cs
+++ Assets/_scripts/MegamanController.cs
@@ -6,16 +6,24 @@ public class MegamanController : MonoBehaviour {
 
     Animator anim;
     SpriteRenderer sprite;
+    private float speed;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        speed = 4f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.D))
+
+        float axisX = Input.GetAxis("Horizontal");
+        //float axisY = Input.GetAxis("Vertical");
+
+        sprite.transform.Translate(new Vector2(axisX, 0) * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.D))
         {
             anim.SetInteger("State", 1);
             sprite.flipX = false;
@@ -24,6 +32,7 @@ public class MegamanController : MonoBehaviour {
         {
             anim.SetInteger("State", 0);
         }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             anim.SetInteger("State", 1);
@@ -33,5 +42,14 @@ public class MegamanController : MonoBehaviour {
         {
             anim.SetInteger("State", 0);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetInteger("State", 2);
+        }else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("State", 0);
+        }
+        
     }
 }
