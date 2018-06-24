3f099fd55c7533c15cc535d77f157f5834565749
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-24 11:00:13 -0400

    Smoother animation transistions for the Megaman Sprite. Working on animation transistions when the sprite collides with an object

--- Assets/_scripts/MegamanController.cs
+++ Assets/_scripts/MegamanController.cs
@@ -7,46 +7,52 @@ public class MegamanController : MonoBehaviour {
     Animator anim;
     SpriteRenderer sprite;
     float speed, axisX;
-    bool flip;
+    bool flip,isJumping;
     int jump;
 
 	void Start () {
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         speed = 4f;
-        flip = false;
+        flip = isJumping = false;
         jump = 1250;
 	}
 	
-    void MovePlayer()
+    private void MovePlayer()
     {
         //Moves the sprite left and right
         axisX = Input.GetAxis("Horizontal");
 
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            isJumping = true;
             Jump();
-            anim.SetInteger("State", 2);
+            anim.SetTrigger("Jump");
+        }
+        else if (!isJumping)
+        {
+
+            //Keeps track of key inputs to switch to certain animations
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                anim.SetTrigger("Walk");
+            }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                anim.SetTrigger("Idle");
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                anim.SetTrigger("Walk");
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                anim.SetTrigger("Idle");
+            }
         }
 
-        //Keeps track of key inputs to switch to certain animations
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.SetInteger("State", 1);
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            anim.SetInteger("State", 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anim.SetInteger("State", 1);
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            anim.SetInteger("State", 0);
-        }
 
         //Flips the sprite depending on the direction it's going
         if (axisX <0.0f && flip ==false)Flip();
@@ -55,19 +61,28 @@ public class MegamanController : MonoBehaviour {
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(axisX * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 
-    void Flip()
+    private void Flip()
     {
         flip = !flip;
         sprite.flipX = flip;
     }
 
-    void Jump()
+    private void Jump()
     {
         gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump);
     }
 
-	// Update is called once per frame
-	void Update () {
+  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isJumping)
+        {
+            isJumping = !isJumping;
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
         MovePlayer();
     }
 }
